00b950027fc83988e96fa0941fa99f6b4a670095
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-01-27 17:35:32 -0600

    Finished specs

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -17,11 +17,20 @@ namespace TicTacToe
         {
             do
             {
+                playerTurn = (playerTurn == "X") ? "O" : "X"; ;
                 DrawBoard();
                 GetInput();
 
             } while (!CheckForWin() && !CheckForTie());
-
+            DrawBoard();
+            if (CheckForWin())
+            {
+                System.Console.WriteLine(playerTurn + " wins!");
+            }
+            else
+            {
+                System.Console.WriteLine("It's a tie!");
+            }
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
@@ -40,55 +49,50 @@ namespace TicTacToe
         public static void PlaceMark(int row, int column)
         {
             // your code goes here
-            DrawBoard();
-            board[row][column] = playerTurn = (playerTurn == "X") ? "O" : "X"; ;
-            DrawBoard();
+            board[row][column] = playerTurn;
+
         }
 
         public static bool CheckForWin()
         {
-            // your code goes here
-
-            return false;
+            return (HorizontalWin() || VerticalWin() || DiagonalWin());
         }
 
         public static bool CheckForTie()
         {
             // your code goes here
-
-            return false;
+            return (!CheckForWin() && board[0][0] != " " && board[0][1] != " " && board[0][2] != " "
+                && board[1][0] != " " && board[1][1] != " " && board[1][2] != " "
+                && board[2][0] != " " && board[2][1] != " " && board[2][2] != " ");
         }
 
         public static bool HorizontalWin()
         {
             // your code goes here
+            // return false;
+            bool row1 = (board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn);
+            bool row2 = (board[1][0] == playerTurn && board[1][1] == playerTurn && board[1][2] == playerTurn);
+            bool row3 = (board[2][0] == playerTurn && board[2][1] == playerTurn && board[2][2] == playerTurn);
 
-            if (board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn)
-            {
-                return (board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn);
-            }
-            else if (board[1][0] == playerTurn && board[1][1] == playerTurn && board[1][2] == playerTurn)
-            {
-                return (board[1][0] == playerTurn && board[1][1] == playerTurn && board[1][2] == playerTurn);
-            }
-            else if (board[2][0] == playerTurn && board[2][1] == playerTurn && board[2][2] == playerTurn)
-            {
-                return (board[2][0] == playerTurn && board[2][1] == playerTurn && board[2][2] == playerTurn);
-            }
+            return (row1 || row2 || row3);
         }
 
         public static bool VerticalWin()
         {
             // your code goes here
-
-            return (board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn);
+            bool column1 = (board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn);
+            bool column2 = (board[0][1] == playerTurn && board[1][1] == playerTurn && board[2][1] == playerTurn);
+            bool column3 = (board[0][2] == playerTurn && board[1][2] == playerTurn && board[2][2] == playerTurn);
+            return (column1 || column2 || column3);
         }
 
         public static bool DiagonalWin()
         {
             // your code goes here
+            bool diagonal1 = (board[0][0] == playerTurn && board[1][1] == playerTurn && board[2][2] == playerTurn);
+            bool diagonal2 = (board[0][2] == playerTurn && board[1][1] == playerTurn && board[2][0] == playerTurn);
 
-            return false;
+            return (diagonal1 || diagonal2);
         }
 
         public static void DrawBoard()
