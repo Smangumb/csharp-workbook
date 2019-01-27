8c2c0fc0eb4b9bbc688faf4505ea8279b1506d49
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-01-27 15:28:04 -0600

    Finished the assignment

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "";
+            string job = "";
+            string school = "";
+
+            Console.WriteLine("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your desired occupation: ");
+            job = (Console.ReadLine());
+            Console.WriteLine("Please enter where you to go school : ");
+            school = (Console.ReadLine());
+
+            Console.WriteLine("Hello! My name is {0} and I am interested in becoming a {1}. I am attending {2} to pursue my career.", name, job, school);
         }
     }
 }
