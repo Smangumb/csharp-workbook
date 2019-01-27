3c7fa0d4faa8ca2fbdb529edd897537632a35666
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-01-27 15:09:40 -0600

    added array

---
+++ sort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 7, 4, 11, 55, 88, 15, 30, 588, 362 };
+
+            for (int i = 0;)
+            //Sort Lowest to Highest
+        }
+    }
+}
