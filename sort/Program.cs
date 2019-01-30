a98c778d1d09d60d4d8a383986e5f5074fe1dd28
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-01-30 11:58:17 -0600

    Enter values for array and sorts lowest to highest

--- sort/Program.cs
+++ sort/Program.cs
@@ -6,13 +6,34 @@ namespace sort
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[]
+            int i;
+            int[] a = new int[30];
+            Console.Write("Enter the Number of values to be Sort : ");
+            // read the string value and convert it in to integer  
+            int n = Convert.ToInt16(Console.ReadLine());
+            //Reading the values one by one  
+            for (i = 1; i <= n; i++)
             {
-                2, 7, 4, 11, 55, 88, 15, 30, 588, 362
-            };
-            for (int i = 0; i < numbers.Length; i++)
+                Console.Write("Enter the No " + i + ":");
+                // read the string value and convert it in to integer  
+                a[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            for (i = 1; i <= n; i++)
             {
-
+                for (int j = 1; j <= n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            Console.Write("Ascending Sort : ");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write(a[i] + " ");
             }
             //Sort Lowest to Highest
         }
