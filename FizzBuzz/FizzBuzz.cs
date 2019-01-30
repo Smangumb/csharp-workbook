9273f0c79fdc829e919f9eb0a3ac33b4c90a98f6
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-01-30 12:04:03 -0600

    Finished

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,29 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                //Checks if divisible by 3 and 5
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                //Checks if divisible by 3
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                //Checks if divisible by 5
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                //If neither print number
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
