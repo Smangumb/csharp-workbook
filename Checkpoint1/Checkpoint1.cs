ebca8d6be033388fc8069e698053be66657df032
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-02-04 12:04:54 -0600

    Finished all 5 programs

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Checkpoint1
 {
@@ -6,7 +7,84 @@ namespace Checkpoint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Program 1
+            System.Console.WriteLine("Total amount of numbers between 1 and 100 that are divisible by 3 with no remainder:");
+            Console.WriteLine(CountThrees());
+
+            //Program 2
+            string InputStr;
+            int InputInt = 0, tempint = 0;
+            do
+            {
+                System.Console.WriteLine("Enter a number or type ok to exit");
+                InputStr = Console.ReadLine();
+                bool success = int.TryParse(InputStr, out tempint);
+                if (success == true)
+                {
+                    InputInt += tempint;
+                }
+                if (success == false)
+                {
+                    break;
+                }
+                Console.WriteLine($"Sum of the numbers is: {InputInt}");
+            }
+            while (InputStr != "ok");
+
+            //Program 3
+            int i, f = 1, num;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Input a number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+                f = f * i;
+
+            Console.WriteLine("{0}! = {1}", num, f);
+
+            //Program 4
+            System.Console.WriteLine("A random number is being picked between 1-10");
+            System.Console.WriteLine("You have 4 tries to guess the number");
+            int answer = 0;
+            int guess = 0;
+            Random number = new Random();
+            answer = number.Next(1, 11);
+            for (int numrandom = 0; numrandom <= 4; numrandom++)
+            {
+                if (numrandom == 4)
+                {
+                    System.Console.WriteLine("You lost");
+                    break;
+                }
+                System.Console.WriteLine("Enter guess");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == answer)
+                {
+                    System.Console.WriteLine("You Won!");
+                    break;
+                }
+            }
+
+            //Program 5
+            Console.WriteLine("Enter some numbers seperated by ", "");
+            var numbers = Console.ReadLine();
+            var splitNumber = numbers.Split(',');
+            var maxNumber = splitNumber.Max();
+            Console.WriteLine("Highest is: " + maxNumber);
+        }
+
+        static int CountThrees()
+        {
+            int count = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+
+            }
+            return count;
         }
     }
 }
