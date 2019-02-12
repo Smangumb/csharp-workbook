2f08c38c03e83604f19134fb58a08530f8416c8e
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-02-11 19:29:40 -0600

    Completed all Projects

--- OOP1/Rainforest/Program.cs
+++ OOP1/Rainforest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Rainforest
 {
@@ -6,7 +8,104 @@ namespace Rainforest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Company rainForest = new Company("RainForest");
+            Warehouse w1 = new Warehouse("Austin", 10);
+            Warehouse w2 = new Warehouse("Houston", 15);
+            rainForest.Warehouses.Add(w1);
+            rainForest.Warehouses.Add(w2);
+            Container austin1 = new Container("Austin-1", 3, 0);
+            Container austin2 = new Container("Austin-2", 3, 1);
+            Container houston1 = new Container("Houston-1", 3, 2);
+            Container houston2 = new Container("Houston-2", 3, 3);
+            w1.Containers.Add(austin1);
+            w1.Containers.Add(austin2);
+            w2.Containers.Add(houston1);
+            w2.Containers.Add(houston2);
+            Item bananas = new Item("Bananas", 2.99);
+            Item apples = new Item("Apples", 1.50);
+            Item watermelon = new Item("Walermelon", 4.99);
+            Item grapes = new Item("grapes", 1.99);
+            austin1.Items.Add(bananas);
+            austin2.Items.Add(apples);
+            houston1.Items.Add(watermelon);
+            houston2.Items.Add(grapes);
+            Dictionary<string, string> index = new Dictionary<string, string>();
+
+            System.Console.WriteLine(String.Format("{0} Company Manifest:", rainForest.Name));
+
+
+            foreach (var warehouse in rainForest.Warehouses)
+            {
+                System.Console.WriteLine(String.Format("This is the location of the Warehouse: {0}", warehouse.location));
+                foreach (var container in warehouse.Containers)
+                {
+                    System.Console.WriteLine(String.Format("The container in this Warehouse: {0}", container.name));
+                    foreach (var item in container.Items)
+                    {
+                        index.Add(item.name, container.name);
+                        System.Console.WriteLine(String.Format("The item in the Container: {0} = ${1}", item.name, item.price));
+                    }
+                }
+            }
+            System.Console.WriteLine("Hey what do you want to look for?");
+            string userinput = Console.ReadLine().ToLower();
+
+
+            foreach (var i in index)
+            {
+                if (userinput == i.Key.ToLower())
+                {
+                    System.Console.WriteLine("Here is where they are: {0}", i.Value);
+                }
+            }
+        }
+    }
+
+    class Company
+    {
+        public string Name { get; set; }
+        public List<Warehouse> Warehouses = new List<Warehouse>();
+        public Company(string name)
+        {
+            this.Name = name;
+        }
+
+    }
+
+    class Warehouse
+    {
+        public string location { get; set; }
+        public int size { get; set; }
+        public List<Container> Containers = new List<Container>();
+        public Warehouse(string location, int size)
+        {
+            this.location = location;
+            this.size = size;
+        }
+    }
+
+    class Container
+    {
+        public string name { get; set; }
+        public int size { get; set; }
+        public int id { get; set; }
+        public List<Item> Items = new List<Item>();
+        public Container(string name, int size, int id)
+        {
+            this.name = name;
+            this.size = size;
+            this.id = id;
+        }
+    }
+
+    class Item
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public Item(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
         }
     }
 }
--- OOP1/StarWars/Program.cs
+++ OOP1/StarWars/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 public class Program
 {
@@ -6,9 +8,23 @@ public class Program
     {
         Person leia = new Person("Leia", "Organa", "Rebel");
         Person darth = new Person("Darth", "Vader", "Imperial");
+        Person luke = new Person("Luke", "Skywalker", "Rebel");
+        Person solo = new Person("Han", "Solo", "Rebel");
+        Person vader = new Person("Darth", "Vader", "Imperial");
+        Person emperor = new Person("Emperor", "Palpatine", "Imperial");
         Ship falcon = new Ship("Rebel", "Smuggling", 2);
         Ship tie = new Ship("Tie", "Fighter", 1);
-        Console.WriteLine("Hello world!");
+        Ship wing = new Ship("X-Wing", "Fighter", 3);
+        Station rebel = new Station("Rebel Space Station", "Rebel", 15);
+        Station deathStar = new Station("Death Star", "Imperial", 100);
+        Station cloudCity = new Station("CloudCity", "Imperial", 25);
+
+        falcon.EnterShip(leia, 0);
+        falcon.EnterShip(solo, 1);
+
+        Console.WriteLine(luke.FullName);
+        cloudCity.dockShip(falcon);
+        Console.WriteLine(cloudCity.shipRoster);
     }
 }
 
@@ -42,11 +58,12 @@ class Person
 
 class Ship
 {
-    private Person[] passengers;
-    public Ship(string alliance, string type, int size)
+    public Person[] passengers;
+    public string name { get; set; }
+    public Ship(string shipName, string type, int size)
     {
         this.Type = type;
-        this.Alliance = alliance;
+        this.name = shipName;
         this.passengers = new Person[size];
     }
 
@@ -70,7 +87,6 @@ class Ship
             {
                 Console.WriteLine(String.Format("{0}", person.FullName));
             }
-
             return "That's Everybody!";
         }
     }
@@ -83,5 +99,47 @@ class Ship
     public void ExitShip(int seat)
     {
         this.passengers[seat] = null;
+    }
+}
+
+class Station
+{
+    public string shipRoster
+    {
+        get
+        {
+            foreach (var ship in ships)
+            {
+                if (ship != null)
+                {
+                    Console.WriteLine(String.Format("This is a {0} ship. Here are the passengers:", ship.name));
+                    Console.WriteLine(ship.Passengers);
+                }
+            }
+
+            return "Roster Complete";
+        }
+    }
+    public void dockShip(Ship ship)
+    {
+        for (int i = 0; i < ships.Length; i++)
+        {
+            if (ships[i] == null)
+            {
+                ships[i] = ship;
+                break;
+            }
+        }
+    }
+    Ship[] ships;
+    public string stationName;
+    private string alliance;
+    private int size;
+    public Station(string stationName, string alliance, int size)
+    {
+        this.stationName = stationName;
+        this.alliance = alliance;
+        this.size = size;
+        this.ships = new Ship[size];
     }
 }
