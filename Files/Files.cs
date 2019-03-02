bfd0a24ecc09d9ef3703f4b4759af883de827c61
Sam Mangum-Bostick <spmb3434@gmail.com>
2019-03-02 11:35:20 -0600

    Progress

--- Files/Files.cs
+++ Files/Files.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Files
 {
@@ -7,6 +8,41 @@ namespace Files
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Createfile();
+        }
+        public static string Createfile()
+        {
+            string fileName = "This is a text file";
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    sw.WriteLine("This is a text file");
+                }
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+                    string s = sr.ReadLine();
+                    while (s != null)
+                    {
+                        Console.WriteLine(s);
+                        s = sr.ReadLine()
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return fileName;
+        }
+        public EditFile()
+        {
+
         }
     }
 }
+
---
+++ Whiteboard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Whiteboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.Console.WriteLine("Dog, Cat, Deer");
+            // System.Console.WriteLine("One of these things is not like the others, one of these things just doesn't belong:");
+            List<string> test = new List<string>();
+            test.Add("dog");
+            test.Add("cat");
+            test.Add("deer");
+            Filter(test).ForEach(Console.WriteLine);
+        }
+
+        public static IEnumerable<string> Filter(IEnumerable input)
+        {
+            List<string> ReturnMe = new List<string>();
+            foreach (string item in input)
+            {
+
+                if (item.Length > 3)
+                {
+                    yield return item;
+                    ReturnMe.Add(item);
+                }
+                // yield return ReturnMe;
+            }
+            // return ReturnMe;
+        }
+    }
+}
